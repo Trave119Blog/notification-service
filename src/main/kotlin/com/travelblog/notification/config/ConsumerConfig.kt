b2ae76052420d26d6faf6437ee0388955e3606a8
Malchanau <yauhenmalchanau@gmail.com>
2024-03-21 20:33:45 +0200

    [CU-869443kqp] Notification service initialization

---
+++ src/main/kotlin/com/travelblog/notification/config/ConsumerConfig.kt
@@ -0,0 +1,24 @@
+package com.travelblog.notification.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.function.Consumer
+
+@Configuration
+class ConsumerConfig {
+
+    @Bean
+    fun customTopic(): Consumer<Post> {
+        return Consumer<Post> { post ->
+            println("Received: $post")
+        }
+    }
+
+    class Post {
+        var postId: String? = null
+        var title: String? = null
+        override fun toString(): String {
+            return "Post(postId=$postId, title=$title)"
+        }
+    }
+}
---
+++ src/test/kotlin/com/travelblog/notification/NotificationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.travelblog.notification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
